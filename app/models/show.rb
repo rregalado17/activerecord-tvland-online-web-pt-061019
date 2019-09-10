7ef0af07338a2e8a170765a40c8a8cf60f8fb53a
Robinson Regalado <rregalado17@gmail.com>
2019-09-10 04:05:02 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,8 +3,8 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   
   def actors_list
-    # return an array of strings containing every musician's name
-    self.artists.collect do |t|
+
+    self.actors.full_name.collect do |t|
       t.name
     end
   end
