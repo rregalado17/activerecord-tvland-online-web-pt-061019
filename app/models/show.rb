57ce399dc75071ab6aa8ef67fd8b45a16c58b9e9
Robinson Regalado <rregalado17@gmail.com>
2019-09-10 04:11:26 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,7 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
-  has_many :network
+  has_many :networks
   
   def actors_list
     self.actors.collect do |t|
